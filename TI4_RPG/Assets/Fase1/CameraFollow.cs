21ad691c0dbd4121888713b6b2b5622ae6ec5d0d
Laprinhas <145940041+Laprinhas@users.noreply.github.com>
2024-04-30 09:29:43 -0300

    Fase 1

---
+++ TI4_RPG/Assets/Fase1/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject cam;
+    public int xCamPre, yCamPre, zCamPre, xCamPos, yCamPos, zCamPos;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if(cam.transform.position == new Vector3 (xCamPre, yCamPre, zCamPre))
+            {
+                cam.transform.position = new Vector3 (xCamPos, yCamPos , zCamPos);
+                Debug.Log("Foi para o pós");
+            } else if(cam.transform.position == new Vector3 (xCamPos, yCamPos, zCamPos))
+            {
+                cam.transform.position = new Vector3 (xCamPre, yCamPre, zCamPre);
+                Debug.Log("Foi para a pré");
+            }
+        }
+        if(cam.transform.position == new Vector3(30, -22, -45))
+        {
+            player.transform.position = new Vector3(30, -45, -30);
+        }
+    }
+}
