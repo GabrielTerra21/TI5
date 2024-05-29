729d7daac3e15f809d291ff36d0c5791d8a5ff5f
Laprinhas <145940041+Laprinhas@users.noreply.github.com>
2024-05-29 16:16:45 -0300

    Fase 1 tá quase, eu juro

--- TI4_RPG/Assets/Scenes/Fase1/LogRise.cs
+++ TI4_RPG/Assets/Scenes/Fase1/LogRise.cs
@@ -8,12 +8,20 @@ public class LogRise : MonoBehaviour
     public GameObject logAfter;
     List<GameObject> controller = new List<GameObject>();
     private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Enemy"))
+        {
+            return;
+        }
         controller.Add(other.gameObject);
         logBefore.SetActive(false);
         logAfter.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Enemy"))
+        {
+            return;
+        }
         controller.Remove(other.gameObject);
         if(controller.Count == 0){
             logAfter.SetActive(false);
