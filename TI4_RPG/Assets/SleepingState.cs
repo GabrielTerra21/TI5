36f4382ab9b2ee1d8dabe29ad4614ee28393f3f2
Pedro de Lima Birchal <birchalpedro@gmail.com>
2024-06-30 17:16:15 -0300

    Dragon anim Update

---
+++ TI4_RPG/Assets/SleepingState.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SleepingState : State
+{
+    private int animationLayerIndex;
+    private Animator animator;
+    
+    
+    private void Awake(){
+        animationLayerIndex = animator.GetLayerIndex("Sleeping");
+    }
+    public override State OnEnterState() {
+        animator.SetLayerWeight(animationLayerIndex, 1);
+        return this;
+    }
+
+    public override void OnExitState() {
+        animator.SetLayerWeight(animationLayerIndex, 0);
+    }
+    
+}
