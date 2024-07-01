55e242d299aceb2a5c86fd8e35ab88d46b12668e
Laprinhas <145940041+Laprinhas@users.noreply.github.com>
2024-06-30 21:01:59 -0300

    Tentativas e falhas

--- TI4_RPG/Assets/Scripts/Enviorment/AnimationTreeFalling.cs
+++ TI4_RPG/Assets/Scripts/Enviorment/AnimationTreeFalling.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationTreeFalling : MonoBehaviour
 {
     private Animator treeAnimator;
+    public GameObject colliderBoss;
     void Start()
     {
         treeAnimator = GetComponent<Animator>();
@@ -12,9 +13,12 @@ public class AnimationTreeFalling : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Colidiu");
         if (other.CompareTag("FireBall"))
         {
+            Debug.Log("Com a bola de fogo");
             FallTree();
+            colliderBoss.SetActive(false);
         }
     }
 
