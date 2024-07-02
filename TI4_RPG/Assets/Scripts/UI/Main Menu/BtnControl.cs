ebacee10689ce9adfa4dcf83a162af3dc3aaa1b5
Pedro de Lima Birchal <birchalpedro@gmail.com>
2024-07-02 05:01:52 -0300

    Dragon UI Fix / Game over Fix / Inventory Fix

--- TI4_RPG/Assets/Scripts/UI/Main Menu/BtnControl.cs
+++ TI4_RPG/Assets/Scripts/UI/Main Menu/BtnControl.cs
@@ -1,14 +1,8 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class BtnControl : MonoBehaviour
 {
-    public void BtnMudaCena(string nomeCena){
-        SceneManager.LoadScene(nomeCena);
-    }
-
     public void BtnQuit(){
         Application.Quit();
-        Debug.Log("Saiu!!");
     }
 }
