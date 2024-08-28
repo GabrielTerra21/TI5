f632687e1ddf88f7c19776d49592642cebf19c9e
chubuca <85251519+chubuca@users.noreply.github.com>
2024-08-28 20:14:22 -0300

    Troquei a cor da Aoe pq tava dificil de ver

--- TI4_RPG/Assets/Scripts/Character/Enemy/AoESO/AoESO.cs
+++ TI4_RPG/Assets/Scripts/Character/Enemy/AoESO/AoESO.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public abstract class AoESO : ScriptableObject
 {
     public int power;
+    public Vector3 hitbox;
     public abstract void DealDamage(Vector3 center,int power);
 }
--- TI4_RPG/Assets/Scripts/Character/Enemy/AoESO/Line.cs
+++ TI4_RPG/Assets/Scripts/Character/Enemy/AoESO/Line.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/AoE/Line", order = 2)]
 public class Line : AoESO
 {
-    public Vector3 hitBox;
     public override void DealDamage(Vector3 center,int power)
     {
-        Collider[] colliders = Physics.OverlapBox(center, hitBox);
+        Collider[] colliders = Physics.OverlapBox(center, hitbox);
         foreach (Collider c in colliders)
         {
             if (c.CompareTag("Player"))
