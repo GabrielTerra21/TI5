7dcb205d1d54887353a5cd312877ed1dc09900b6
Pedro de Lima Birchal <birchalpedro@gmail.com>
2024-09-20 15:36:10 -0300

    direção da chave aponta pra camera
    Chaves de baú agora sempre apontam para a camera

--- TI4_RPG/Assets/Scripts/Lima/keyAnimation.cs
+++ TI4_RPG/Assets/Scripts/Lima/keyAnimation.cs
@@ -4,4 +4,8 @@ public class keyAnimation : MonoBehaviour {
     public void Deactivate() {
         gameObject.SetActive(false);
     }
+
+    private void OnEnable() {
+        transform.rotation = Quaternion.LookRotation(Camera.main.transform.right, Camera.main.transform.forward);
+    }
 }
