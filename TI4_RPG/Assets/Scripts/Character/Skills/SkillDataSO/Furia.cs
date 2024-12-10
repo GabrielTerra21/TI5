79219861a53bc99d7b1e62e2ac202f01009832fe
chubuca <85251519+chubuca@users.noreply.github.com>
2024-12-09 21:58:36 -0300

    correcoes

--- TI4_RPG/Assets/Scripts/Character/Skills/SkillDataSO/Furia.cs
+++ TI4_RPG/Assets/Scripts/Character/Skills/SkillDataSO/Furia.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Skills/Furia", order = 2)]
 public class Furia : SkillDataSO
@@ -6,7 +7,12 @@ public class Furia : SkillDataSO
     public override void OnCast(Character from, Character target)
     {
         from.ApplyBonusAttack(Power);
+        from.StartCoroutine(Coldonw(from,CastTime));
         //BuffController.instance.ListarBuff(CastTime, "attack", from, Power);
     }
-
+    IEnumerator Coldonw(Character from, float time)
+    {
+        yield return new WaitForSeconds(time);
+        from.ApplyBonusAttack(-Power);
+    }
 }
